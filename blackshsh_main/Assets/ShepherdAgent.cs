5bc3a1b8efe9099879b23a2d03aa5f36106b41d3
Chris <christian.walter@outlook.at>
2018-06-18 00:30:01 +0200

    added cubes

---
+++ blackshsh_main/Assets/ShepherdAgent.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShepherdAgent : Agent {
+
+  [Header("Specific to Wolf")]
+  public GameObject wolf;
+
+  public override void InitializeAgent(){
+
+  }
+
+  public override void CollectObservations(){
+    AddVectorObs(gameObject.transform.rotation.z);
+    AddVectorObs(gameObject.transform.rotation.x);
+    AddVectorObs((wolf.transform.position - gameObject.transform.position));
+    AddVectorObs(wolf.transform.GetComponent<Rigidbody>().velocity);
+    SetTextObs("Testing " + gameObject.GetInstanceID());
+  }
+
+  public override void AgentAction(float[] vectorAction, string textAction){
+
+  }
+
+  public override void AgentReset(){
+
+  }
+}
---
+++ blackshsh_main/Assets/scripts/BlackAcademy.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlackAcademy : Academy {
+
+  public override void AcademyReset(){
+
+  }
+
+  public override void AcademyStep(){
+    
+  }
+}
