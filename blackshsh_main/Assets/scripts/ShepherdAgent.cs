b39348aadb78c4c2b66593c12e3afaa796662065
Chris <christian.walter@outlook.at>
2018-06-21 15:08:15 +0200

    using ml-agent v0.4 now

--- blackshsh_main/Assets/scripts/ShepherdAgent.cs
+++ blackshsh_main/Assets/scripts/ShepherdAgent.cs
@@ -1,11 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using MLAgents;
 
 public class ShepherdAgent : Agent {
 
-  [Header("Specific to Wolf")]
+  [Header("Shepherd Specific")]
   public GameObject wolf;
+  public GameObject white_sheep;
+  public GameObject black_sheep;
+
+  public float speed;
 
   private Vector3 shepherd_start_pos;
 
@@ -14,16 +19,29 @@ public class ShepherdAgent : Agent {
   }
 
   public override void CollectObservations(){
-    /*
-    AddVectorObs(gameObject.transform.rotation.z);
-    AddVectorObs(gameObject.transform.rotation.x);
-    AddVectorObs((wolf.transform.position - gameObject.transform.position));
-    SetTextObs("Testing " + gameObject.GetInstanceID());
-    */
+    AddVectorObs(gameObject.transform.position);
+    AddVectorObs(wolf.transform.position);
+    AddVectorObs(white_sheep.transform.position);
+    AddVectorObs(black_sheep.transform.position);
+    //AddVectorObs((wolf.transform.position - gameObject.transform.position));
+    //Debug.Log("Collect");
   }
 
   public override void AgentAction(float[] vectorAction, string textAction){
-
+    // input vectors
+    if (brain.brainParameters.vectorActionSpaceType == SpaceType.continuous){
+      float x = Mathf.Clamp(vectorAction[0], -1, 1);
+      float z = Mathf.Clamp(vectorAction[1], -1, 1);
+      float run = Mathf.Clamp(vectorAction[2], 1, 2);
+      // actions
+      float action_x = x * speed * run;
+      float action_z = z * speed * run;
+      // move the agent
+      if (action_x != 0 || action_z != 0){
+        Debug.Log("Translate shepherd");
+        gameObject.transform.Translate(action_x, 0, action_z); 
+      }
+    }
   }
 
   public override void AgentReset(){
