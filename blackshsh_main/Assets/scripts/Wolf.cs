1e56beb6f47943570e9d4112a0db8d1d6e1d45b7
Chris <christian.walter@outlook.at>
2018-06-24 15:56:37 +0200

    wolf follows also shepherd and white sheep

--- blackshsh_main/Assets/scripts/Wolf.cs
+++ blackshsh_main/Assets/scripts/Wolf.cs
@@ -9,6 +9,9 @@ public class Wolf : MonoBehaviour {
   public Transform white_sheep;
   public Transform shepherd;
 
+  public float follow_shepherd;
+  public float follow_white_sheep;
+
   private Vector3 wolf_start_pos;
 
   NavMeshAgent agent;
@@ -22,7 +25,21 @@ public class Wolf : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+    // calculate distances
+    Vector3 wolf_pos = gameObject.GetComponent<Transform>().position;
+    float shepherd_dist = Vector3.Distance(wolf_pos, shepherd.position);
+		float white_sheep_dist = Vector3.Distance(wolf_pos, white_sheep.position);
+    Debug.Log("distance: " + shepherd_dist + white_sheep_dist);
+    // follow closest object
+    if (white_sheep_dist < follow_white_sheep){
+      agent.SetDestination(white_sheep.position);
+    }
+    else if (shepherd_dist < follow_shepherd){
+      agent.SetDestination(shepherd.position);
+    }
+    else{
+      agent.SetDestination(black_sheep.position);
+    }
 	}
 
   // Collision
